1fbfdb12114557f202a1bdb4c73333d07a6db63b
FILIP PC <01140724@pw.edu.pl>
2019-11-03 17:06:08 +0100

    Initial commit

---
+++ src/com/filipzarnowiec/Calculator.java
@@ -0,0 +1,22 @@
+package com.filipzarnowiec;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JButton a1Button;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a8Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton clearButton;
+    private JButton a0Button;
+    private JButton button2;
+    private JButton button3;
+}
---
+++ src/com/filipzarnowiec/Main.java
@@ -0,0 +1,8 @@
+package com.filipzarnowiec;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    UI app = new UI();
+    }
+}
